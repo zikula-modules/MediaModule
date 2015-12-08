896e016a244c4ce75b08a0221104f49109b9c789
Christian Flach <cmfcmf@users.noreply.github.com>
2015-12-08 20:01:41 +0100

    Also create the other sortable only if there is something to sort.
    @rallek

--- Resources/public/js/Collection/sort.js
+++ Resources/public/js/Collection/sort.js
@@ -1,19 +1,21 @@
 (function ($) {
     $(function () {
         var mediaContainer = document.getElementById("cmfcmfmedia-media-sortable-container");
-        Sortable.create(mediaContainer, {
-            animation: 150,
-            handle: ".fa-sort",
-            draggable: ".sortable",
-            onUpdate: function (evt) {
-                $.get(Routing.generate('cmfcmfmediamodule_media_reorder'), {
-                    id: $(evt.item).data('id'),
-                    position: evt.newIndex
-                }).success(function () {
-                    window.toastr['success']('', 'Saved new position.');
-                }).fail(window.CmfcmfMediaModule.Util.Ajax.fail);
-            }
-        });
+        if (mediaContainer != null) {
+            Sortable.create(mediaContainer, {
+                animation: 150,
+                handle: ".fa-sort",
+                draggable: ".sortable",
+                onUpdate: function (evt) {
+                    $.get(Routing.generate('cmfcmfmediamodule_media_reorder'), {
+                        id: $(evt.item).data('id'),
+                        position: evt.newIndex
+                    }).success(function () {
+                        window.toastr['success']('', 'Saved new position.');
+                    }).fail(window.CmfcmfMediaModule.Util.Ajax.fail);
+                }
+            });
+        }
         var collectionContainer = document.getElementById('cmfcmfmedia-collection-sortable-container');
         if (collectionContainer != null) {
             Sortable.create(collectionContainer, {
