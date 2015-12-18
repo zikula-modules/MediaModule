463dcbe1e5b7e515826291ce88c1a4ebd29241a9
Christian Flach <cmfcmf.flach@gmail.com>
2015-12-18 22:16:43 +0100

    Simplify things a little.

--- Form/Permission/GroupPermissionType.php
+++ Form/Permission/GroupPermissionType.php
@@ -3,6 +3,7 @@
 namespace Cmfcmf\Module\MediaModule\Form\Permission;
 
 use Symfony\Component\Form\FormBuilderInterface;
+use Zikula\GroupsModule\Entity\GroupEntity;
 
 class GroupPermissionType extends AbstractPermissionType
 {
@@ -10,14 +11,23 @@ class GroupPermissionType extends AbstractPermissionType
     {
         parent::buildForm($builder, $options);
 
+        /** @var GroupEntity[]|false $groups */
+        $groups = \ModUtil::apiFunc('ZikulaGroupsModule', 'user', 'getall');
+        if ($groups === false) {
+            $groups = [];
+        }
+
+        $choices = [];
+        $choices[-1] = $this->__('All groups');
+
+        foreach ($groups as $group) {
+            $choices[$group->getGid()] = $group->getName();
+        }
+
         $builder->add('groupIds', 'choice', [
             'label' => $this->__('Groups'),
             'multiple' => true,
-            'choices' => [
-                '-1' => 'all groups',
-                '1' => 'one group',
-                '2' => 'another group'
-            ]
+            'choices' => $choices
         ]);
     }
 }
