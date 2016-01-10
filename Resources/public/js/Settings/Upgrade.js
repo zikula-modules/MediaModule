2299ff4266d95c917b17ea12a223f129ef9db0fe
Christian Flach <cmfcmf.flach@gmail.com>
2016-01-10 12:40:09 +0100

    Improve upgrade error handling.

--- Resources/public/js/Settings/Upgrade.js
+++ Resources/public/js/Settings/Upgrade.js
@@ -1,44 +1,48 @@
 (function ($) {
-    function executeStep(step) {
-        var steps = $('#cmfcmfmedia-settings-upgrade-start-btn').data('steps');
-        if (steps == step) {
-            window.location.href = Routing.generate('cmfcmfmediamodule_settings_settings');
-            return;
-        }
-
-        var $stepsList = $('#cmfcmfmedia-settings-upgrade-steps-list');
-        var $li = $stepsList.find('li:nth-child(' + (step + 1) + ')');
-        var $icon = $li.find('.fa');
-
-        $li.removeClass('text-muted').addClass('text-primary');
-        $icon.addClass('fa-spin');
-
-        $.ajax(Routing.generate('cmfcmfmediamodule_upgrade_ajax', {step: $li.data('step')}), {
-            timeout: 60000
-        })
-            .fail(function () {
-                $li.addClass('text-danger');
-            })
-            .done(function (result) {
-                if (result.proceed) {
-                    $li.addClass('text-success');
-                    executeStep(step + 1);
-                } else {
-                    $li.addClass('text-danger');
-                    alert(result.message);
-                }
-            })
-            .always(function () {
-                $icon.removeClass('fa-spin');
-            })
-        ;
-    }
-
     $(function () {
-        $('#cmfcmfmedia-settings-upgrade-start-btn').one('click', function () {
+        var $upgradeButton = $('#cmfcmfmedia-settings-upgrade-start-btn');
+
+        $upgradeButton.one('click', function () {
             $(this).attr('disabled', true);
             $(this).find('.fa').removeClass('hidden');
             executeStep(0);
         });
+
+        function executeStep(step) {
+            var steps = $('#cmfcmfmedia-settings-upgrade-start-btn').data('steps');
+            if (steps == step) {
+                window.location.href = Routing.generate('cmfcmfmediamodule_settings_settings');
+                return;
+            }
+
+            var $stepsList = $('#cmfcmfmedia-settings-upgrade-steps-list');
+            var $li = $stepsList.find('li:nth-child(' + (step + 1) + ')');
+            var $icon = $li.find('.fa');
+
+            $li.removeClass('text-muted').addClass('text-primary');
+            $icon.addClass('fa-spin');
+
+            $.ajax(Routing.generate('cmfcmfmediamodule_upgrade_ajax', {step: $li.data('step')}), {
+                timeout: 60000
+            })
+                .fail(function () {
+                    $li.addClass('text-danger');
+                })
+                .done(function (result) {
+                    if (result.proceed) {
+                        $li.addClass('text-success');
+                        executeStep(step + 1);
+                    } else {
+                        $li.addClass('text-danger');
+                        $upgradeButton.removeClass('btn-primary').addClass('btn-danger');
+                        $upgradeButton.text('Upgrade failed.');
+                        alert(result.message);
+                    }
+                })
+                .always(function () {
+                    $icon.removeClass('fa-spin');
+                })
+            ;
+        }
     });
 })(jQuery);
--- Upgrade/ModuleUpgrader.php
+++ Upgrade/ModuleUpgrader.php
@@ -11,6 +11,8 @@
 
 namespace Cmfcmf\Module\MediaModule\Upgrade;
 
+use Cmfcmf\Module\MediaModule\Exception\UpgradeFailedException;
+use Github\Exception\RuntimeException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Translation\TranslatorInterface;
 use Zikula\Bundle\CoreBundle\CacheClearer;
@@ -69,18 +71,96 @@ class ModuleUpgrader
         $this->moduleDir = rtrim($this->moduleDir, '/\\');
     }
 
+    public function getUpgradeSteps()
+    {
+        return [
+            'php-check' => [
+                'text' => $this->translator->trans('Checking server requirements', [], 'cmfcmfmediamodule'),
+                'icon' => 'fa-server'
+            ],
+            'version-check' => [
+                'text' => $this->translator->trans('Checking installed and available versions', [], 'cmfcmfmediamodule'),
+                'icon' => 'fa-github'
+            ],
+            'permission-check' => [
+                'text' => $this->translator->trans('Checking file system permissions', [], 'cmfcmfmediamodule'),
+                'icon' => 'fa-files-o'
+            ],
+            'download' => [
+                'text' => $this->translator->trans('Downloading new version', [], 'cmfcmfmediamodule'),
+                'icon' => 'fa-download'
+            ],
+            'extracting' => [
+                'text' => $this->translator->trans('Extracting new version', [], 'cmfcmfmediamodule'),
+                'icon' => 'fa-file-archive-o'
+            ],
+            'upgrading' => [
+                'text' => $this->translator->trans('Running upgrade', [], 'cmfcmfmediamodule'),
+                'icon' => 'fa-code'
+            ],
+        ];
+    }
+
+    /**
+     * Execute the upgrade.
+     *
+     * @param string $step The step to execute.
+     * @param VersionChecker $versionChecker
+     *
+     * @return bool Whether or not the upgrade is done.
+     */
+    public function upgrade($step, VersionChecker $versionChecker)
+    {
+        switch ($step) {
+            case 'php-check':
+                $this->checkRequirements();
+
+                return false;
+            case 'version-check':
+                $this->getReleaseToUpgradeTo($versionChecker);
+
+                return false;
+            case 'permission-check':
+                $this->checkPermissions();
+
+                return false;
+            case 'download':
+                $release = $this->getReleaseToUpgradeTo($versionChecker);
+                $asset = $versionChecker->getZipAssetFromRelease($release);
+                if (!$asset) {
+                    throw new UpgradeFailedException(
+                        $this->translator->trans(
+                            'Something went wrong. The release doesn\'t contain a ZIP asset.',
+                            [],
+                            'cmfcmfmediamodule'));
+                }
+                $this->downloadNewVersion($asset['browser_download_url']);
+
+                return false;
+            case 'extracting':
+                $this->extractNewVersion();
+
+                return false;
+            case 'upgrading':
+                $this->doUpgrade();
+
+                return true;
+            default:
+                throw new \RuntimeException('Invalid upgrade step received!');
+        }
+    }
+
     /**
      * Checks whether the server fulfills all necessary requirements.
      *
      * @return bool|string
      */
-    public function checkRequirements()
+    private function checkRequirements()
     {
-        if (class_exists('ZipArchive') && extension_loaded('curl')) {
-            return true;
+        if (!class_exists('ZipArchive') || !extension_loaded('curl')) {
+            throw new UpgradeFailedException(
+                $this->translator->trans('Please enable the ZIP and CURL PHP extensions', [], 'cmfcmfmediamodule'));
         }
-
-        return $this->translator->trans('Please enable the ZIP and CURL PHP extensions', [], 'cmfcmfmediamodule');
     }
 
     /**
@@ -88,19 +168,26 @@ class ModuleUpgrader
      *
      * @return bool|string
      */
-    public function checkPermissions()
+    private function checkPermissions()
     {
         if (!is_writable($this->moduleDir)) {
-            return $this->translator->trans('Please make %s writable.', ['%s' => $this->moduleDir], 'cmfcmfmediamodule');
+            throw new UpgradeFailedException(
+                $this->translator->trans('Please make %s writable.', ['%s' => $this->moduleDir], 'cmfcmfmediamodule'));
         }
         if (!is_writable($this->moduleDir . '/Controller/MediaController.php')) {
-            return $this->translator->trans('%s is writable but it\'s content is not. Please make sure to make it recursively writable.', ['%s' => $this->moduleDir], 'cmfcmfmediamodule');
+            throw new UpgradeFailedException(
+                $this->translator->trans(
+                    '%s is writable but it\'s content is not. Please make sure to make it recursively writable.',
+                    ['%s' => $this->moduleDir],
+                    'cmfcmfmediamodule'));
         }
         if (!is_writable($this->moduleDir . '/.gitignore')) {
-            return $this->translator->trans('%s is writable but some files are not. Please make sure to make it recursively writable. If you already tried, please try "chmod 777 -R media-module" from within the cmfcmf folder.', ['%s' => $this->moduleDir], 'cmfcmfmediamodule');
+            throw new UpgradeFailedException(
+                $this->translator->trans(
+                    '%s is writable but some files are not. Please make sure to make it recursively writable. If you already tried, please try "chmod 777 -R media-module" from within the cmfcmf folder.',
+                    ['%s' => $this->moduleDir],
+                    'cmfcmfmediamodule'));
         }
-
-        return true;
     }
 
     /**
@@ -110,11 +197,9 @@ class ModuleUpgrader
      *
      * @return bool
      */
-    public function downloadNewVersion($url)
+    private function downloadNewVersion($url)
     {
         file_put_contents($this->cacheFile, fopen($url, 'r'));
-
-        return true;
     }
 
     /**
@@ -122,7 +207,7 @@ class ModuleUpgrader
      *
      * @return bool
      */
-    public function extractNewVersion()
+    private function extractNewVersion()
     {
         // First check if the module dir really is writable.
         $this->fs->touch($this->moduleDir . '/.gitignore');
@@ -139,8 +224,6 @@ class ModuleUpgrader
         $this->fs->remove($this->cacheFile);
 
         $this->cacheClearer->clear('symfony');
-
-        return true;
     }
 
     /**
@@ -148,7 +231,7 @@ class ModuleUpgrader
      *
      * @return bool|string
      */
-    public function upgrade()
+    private function doUpgrade()
     {
         $filemodules = \ModUtil::apiFunc('ZikulaExtensionsModule', 'admin', 'getfilemodules');
         \ModUtil::apiFunc('ZikulaExtensionsModule', 'admin', 'regenerate', ['filemodules' => $filemodules]);
@@ -157,10 +240,44 @@ class ModuleUpgrader
             'id' => \ModUtil::getIdFromName('CmfcmfMediaModule')
         ]);
 
-        if ($worked == true) {
-            return $worked;
+        if ($worked != true) {
+            throw new UpgradeFailedException(
+                $this->translator->trans(
+                    'Something went wrong with the upgrade code. This should not have happened!',
+                    [],
+                    'cmfcmfmediamodule'));
         }
+    }
 
-        return $this->translator->trans('Something went wrong with the upgrade code. This should not have happened!', [], 'cmfcmfmediamodule');
+    /**
+     * @param VersionChecker $versionChecker
+     *
+     * @return array The release to upgrade to.
+     */
+    private function getReleaseToUpgradeTo(VersionChecker $versionChecker)
+    {
+        try {
+            if (!$versionChecker->checkRateLimit()) {
+                throw new UpgradeFailedException(
+                    $this->translator->trans(
+                        'Your GitHub API Rate limit is exceeded. Please try again later.',
+                        [],
+                        'cmfcmfmediamodule'));
+            }
+            $info = \ModUtil::getInfoFromName('CmfcmfMediaModule');
+            $release = $versionChecker->getReleaseToUpgradeTo($info['version']);
+            if ($release === false) {
+                throw new UpgradeFailedException(
+                    $this->translator->trans('No release to upgrade to available!', [], 'cmfcmfmediamodule'));
+            }
+
+            return $release;
+        } catch (RuntimeException $e) {
+            // Something went wrong with the GitHub API.
+            throw new UpgradeFailedException($this->translator->trans(
+                'Could not connect to GitHub. Is the server connected to the internet?',
+                [],
+                'cmfcmfmediamodule'));
+        }
     }
 }
