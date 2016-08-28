08fd0cb7af6088095a8f66c4fdc35bb01e180a88
Christian Flach <cmfcmf.flach@gmail.com>
2016-08-28 13:46:50 +0200

    Fix StyleCI issues.

--- MediaType/UploadableMediaTypeInterface.php
+++ MediaType/UploadableMediaTypeInterface.php
@@ -29,8 +29,8 @@ interface UploadableMediaTypeInterface
      * Whether or not this media type supports uploading the file represented by the file info array.
      *
      * @param string $mimeType The mime type
-     * @param int    $size The file size
-     * @param string $name The file name
+     * @param int    $size     The file size
+     * @param string $name     The file name
      *
      * @return int 10 if it perfectly matches, 0 if it can't upload.
      */
