ddde970aaac7ee7e155c99bb09478a43931e29a0
Christian Flach <cmfcmf.flach@gmail.com>
2016-09-19 11:46:50 +0200

    Add Galleria template.

---
+++ CollectionTemplate/GalleriaTemplate.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the MediaModule for Zikula.
+ *
+ * (c) Christian Flach <hi@christianflach.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cmfcmf\Module\MediaModule\CollectionTemplate;
+
+/**
+ * Displays a Galleria image slider https://galleria.io/
+ */
+class GalleriaTemplate extends AbstractTemplate
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle()
+    {
+        return $this->translator->trans('Galleria image slider', [], 'cmfcmfmediamodule');
+    }
+}
---
+++ Resources/public/js/Media/Display/galleria.js
@@ -0,0 +1,7 @@
+(function ($) {
+    $(function () {
+        Galleria.run('.cmfcmfmedia-display-galleria', {
+            height: 400
+        });
+    });
+})(jQuery);
